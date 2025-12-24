53222f8b5807587c73aa6d7407ac8a2ea6ff1570
Santomassimo85 <santomassimo85@gmail.com>
2025-12-24 18:44:28 +0100

    initial phase

---
+++ src/main/java/com/epicode/GreenTravelPlanner/GreenTravelPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.GreenTravelPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenTravelPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/GreenTravelPlanner/GreenTravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.GreenTravelPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
